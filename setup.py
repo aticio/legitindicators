64d6f092aa30fc51080fd6ae786298f2bcbecffd
Özgür Atıcı <100781@isbank.com.tr>
2020-04-10 14:58:56 +0300

    bug fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as fh:
 
 setup(
     name="legitindicators",
-    version="0.0.12",
+    version="0.0.13",
     description="Legit indicators to be used in trading strategies.",
     packages=find_packages(),
     classifiers=[
