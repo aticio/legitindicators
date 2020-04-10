44e2bdf3979fa8aed67674b9140704cfe42dc6b5
Özgür Atıcı <100781@isbank.com.tr>
2020-04-10 12:32:52 +0300

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as fh:
 
 setup(
     name="legitindicators",
-    version="0.0.10",
+    version="0.0.11",
     description="Legit indicators to be used in trading strategies.",
     package_dir=["": "src"]
     packages=find_packages(),
