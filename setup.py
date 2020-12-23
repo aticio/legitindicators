b45bd427eb946dbbd342569d4cf69ec2509ba055
aticio <aticiozgur@gmail.com>
2020-12-24 00:15:45 +0300

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as fh:
 
 setup(
     name="legitindicators",
-    version="0.0.51",
+    version="0.0.52",
     description="Legit indicators to be used in trading strategies.",
     py_modules=["legitindicators"],
     classifiers=[
